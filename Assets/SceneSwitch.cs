ab07a032232ecdfffcaf594665b080ff11cf7e72
Defucota <defucota@gmail.com>
2022-10-16 23:52:48 +0200

    hryltxwgfdzh<sdh<rs

--- Assets/SceneSwitch.cs
+++ Assets/SceneSwitch.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneSwitch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string scene;
 
-    // Update is called once per frame
-    void Update()
+    public void Switch()
     {
-        
+        SceneManager.LoadScene(scene);
     }
 }
